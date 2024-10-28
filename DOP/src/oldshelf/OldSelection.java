5ca517ba405a5f88b61c9522830d4c59724e59fa
ashwinnair10 <ashwinnair1894@gmail.com>
2024-10-28 23:29:11 +0530

    changed if-else to switch case

--- DOP/src/oldshelf/OldSelection.java
+++ DOP/src/oldshelf/OldSelection.java
@@ -11,31 +11,41 @@ public class OldSelection {
 	 */
 	public static String getAgeOrTitle(Object o) {
 		
-		if (o instanceof Book) {
-			if(o instanceof Comic c){
-				return c.getTitle();
-			}
-			else if(o instanceof Fiction f){
-				return f.getName();
-			}
-			else if(o instanceof TextBook t){
-				return t.subject();
-			}
-		}
-		return null;
+		// if (o instanceof Book) {
+		// 	if(o instanceof Comic c){
+		// 		return c.getTitle();
+		// 	}
+		// 	else if(o instanceof Fiction f){
+		// 		return f.getName();
+		// 	}
+		// 	else if(o instanceof TextBook t){
+		// 		return t.subject();
+		// 	}
+		// }
+		return switch(o){
+			case Comic c->c.getTitle();
+			case Fiction f->f.getName();
+			case TextBook t->t.subject();
+			default->null;
+		};
 	}
 
 	public static void main(String[] args) {
 		
 		// DONE: Write a test code here and execute and text.
-		TextBook t = new TextBook("Mathematics");
+		Object o=new Object();
+		TextBook t=new TextBook("Mathematics");
 		Comic c1=new Comic("Adventures of Spider-man", 12);
-		Comic c2=new Comic("Batman",30);
+		//Comic c2=new Comic("Batman",30);
 		Fiction f=new Fiction("Harry Potter",FictionType.Tragedy);
-		System.out.println(t);
-		System.out.println(c1);
-		System.out.println(c2);
-		System.out.println(f);
-		System.out.println(c1.equals(c2));
+		// System.out.println(t);
+		// System.out.println(c1);
+		// System.out.println(c2);
+		// System.out.println(f);
+		System.out.println(getAgeOrTitle(t));
+		System.out.println(getAgeOrTitle(c1));
+		System.out.println(getAgeOrTitle(f));
+		System.out.println(getAgeOrTitle(o));
+		//System.out.println(c1.equals(c2));
 	}
 }
