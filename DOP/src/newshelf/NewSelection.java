58199a8d8d37137c4dc873d39e3b4f42d5427a04
ashwinnair10 <ashwinnair1894@gmail.com>
2024-10-28 23:37:07 +0530

    used typeswitch and when

--- DOP/src/newshelf/NewSelection.java
+++ DOP/src/newshelf/NewSelection.java
@@ -13,28 +13,37 @@ public class NewSelection {
 	 */
 	public static String getAgeOrTitle(Object o) {
 		
-		if (o instanceof IBook) {
-			if(o instanceof Comic c){
-				return c.Title();
-			}
-			else if(o instanceof Fiction f){
-				return f.name();
-			}
-			else if(o instanceof TextBook t){
-				return t.subject();
-			}
-		}
-		return null;
+		// if (o instanceof IBook) {
+		// 	if(o instanceof Comic c){
+		// 		return c.Title();
+		// 	}
+		// 	else if(o instanceof Fiction f){
+		// 		return f.name();
+		// 	}
+		// 	else if(o instanceof TextBook t){
+		// 		return t.subject();
+		// 	}
+		// }
+		// return null;
+		return switch(o){
+			case Comic c->c.Title();
+			case Fiction f->f.name();
+			case TextBook t->t.subject();
+			case Object obj when obj instanceof TestClass tc->tc.s();
+			default->null;
+		};
 	}
 
 	public static void main(String[] args) {
 		
 		// DONE: Write a test code here and execute and text.
-		TextBook t = new TextBook("Science");
+		TextBook t=new TextBook("Science");
 		Comic c=new Comic("Superman", 32);
 		Fiction f=new Fiction("Maze Runner",FictionType.Tragedy);
+		TestClass tc=new TestClass("Testing");
 		System.out.println(getAgeOrTitle(t));
 		System.out.println(getAgeOrTitle(c));
 		System.out.println(getAgeOrTitle(f));
+		System.out.println(getAgeOrTitle(tc));
 	}
 }
