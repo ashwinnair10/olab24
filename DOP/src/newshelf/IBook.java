8cacac75763fdf0689d26f7b77523cfe8ae8ae3c
ashwinnair10 <ashwinnair1894@gmail.com>
2024-10-28 23:36:34 +0530

    added a testclass to check "when" keyword fucntionality

--- DOP/src/newshelf/IBook.java
+++ DOP/src/newshelf/IBook.java
@@ -2,8 +2,10 @@ package newshelf;
 
 import oldshelf.FictionType;
 
-public sealed interface IBook permits Comic,Fiction,TextBook{}
+public sealed interface IBook permits Comic,Fiction,TextBook,TestClass{}
 
 record Comic(String Title,int ageOfMainCharacter) implements IBook{}
 record Fiction(String name,FictionType type) implements IBook{}
 record TextBook(String subject) implements IBook {}
+
+record TestClass(String s) implements IBook{}
